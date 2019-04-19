592fe35b62166836bb6afc738d0cc6d021b141db
JunWenqing <18790339095@sina.cn>
2019-04-19 11:14:18 +0800

    Initial commit

---
+++ src/main/java/dyc/myt/MytApplication.java
@@ -0,0 +1,13 @@
+package dyc.myt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytApplication.class, args);
+    }
+
+}
